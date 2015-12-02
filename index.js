58b60ca429dd12bfe90e84083faa79ec2bfa44d4
Maira Bello <maira.araujo@liferay.com>
2015-12-02 14:43:51 -0300

    Adds a simple initial implementation
    The command given to the cli will be used as the gulp task name, and any arguments will be passed as gulp-metal's options.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+var chalk = require('chalk');
+var gulp = require('gulp');
+var metal = require('gulp-metal');
+var yargs = require('yargs');
+
+var argv = yargs
+  .demand(1, 1)
+  .argv;
+
+metal.registerTasks(argv);
+
+console.log('Starting ' + chalk.cyan('\'' + argv._[0] + '\'') + '...');
+gulp.start(argv._[0], function(error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log('Finished ' + chalk.cyan('\'' + argv._[0] + '\''));
+  }
+});
